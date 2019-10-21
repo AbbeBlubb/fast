3df3a06ce5aa50b17b65893fc3267229c0089d88
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-10-21 22:50:07 +0200

    Accessibility and description

--- src/index.js
+++ src/index.js
@@ -7,16 +7,25 @@ const header = `
   </header>
 `;
 
+// Create description
+const about = `
+  <p class='description'>
+    The images will load when entering the viewport.
+  </p>
+`;
+
 // Create the image section
 const arrayOfImageNames = [];
 for (let i = 1; i <= 20; i++) { arrayOfImageNames.push(`${i}`);}
-const imageElements = arrayOfImageNames.map(element => `<img class='image' src=assets/${element}.png alt='Loaded image'>`).join('');
+const imageElements = arrayOfImageNames.map(element => `<img class='image' src=assets/${element}.png alt='Random photo' title='Random photo'>`).join('');
 
 const imageSection = `
-  <section class='image-section'>
+  <section class='image-section' role='contentinfo' aria-label='Section with photos'>
     ${imageElements}
   </section>
 `;
 
+// Insert sections in the DOM
 document.body.insertAdjacentHTML('afterbegin', header);
+document.querySelector('header').insertAdjacentHTML('afterend', about);
 document.body.insertAdjacentHTML('beforeend', imageSection);

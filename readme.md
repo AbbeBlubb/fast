5015d1845830288d70067e3e01c630558b11c408
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-10-21 21:55:55 +0200

    Create elements and insert into the DOM, CSS

--- readme.md
+++ readme.md
@@ -1,6 +1,11 @@
-Features
+## Features
 
 Webpack
+- webpack dev server
+- babel loader
+- Separate bundles for CSS and JS
+- Hot reload
+
 IntersectionObserve
 System.import()
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import '../styles/lazy-loading.css';
+import '../styles/stylesheet.css';
 
-const hello = () => {
-  console.log("hello411!")
-}
+// Create header
+const header = `
+  <header>
+    <h1>The Lazy Loader</h1>
+  </header>
+`;
 
-hello()
+// Create the image section
+const arrayOfImageNames = [];
+for (let i = 1; i <= 20; i++) { arrayOfImageNames.push(`${i}`);}
+const imageElements = arrayOfImageNames.map(element => `<img class='image' src=assets/${element}.png alt='Loaded image'>`).join('');
+
+const imageSection = `
+  <section class='image-section'>
+    ${imageElements}
+  </section>
+`;
+
+document.body.insertAdjacentHTML('afterbegin', header);
+document.body.insertAdjacentHTML('beforeend', imageSection);

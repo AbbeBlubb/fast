aac0398b52be5b71ba52e9213c213ca068afd15e
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-10-22 22:42:13 +0200

    Refactoring of the HTML strings to separate file

---
+++ src/HTMLString.js
@@ -0,0 +1,27 @@
+// Create header
+const header = `
+  <header>
+    <h1>The Lazy Loader</h1>
+  </header>
+`;
+
+// Create description
+const about = `
+  <p class='description'>
+    The images will load when entering the viewport.
+  </p>
+`;
+
+// Create the image section
+const arrayOfImageNames = [];
+for (let i = 1; i <= 20; i++) { arrayOfImageNames.push(`${i}`);}
+const imageElements = arrayOfImageNames.map(element => `<img class='lazy-image' data-src=assets/${element}.png data-alt='Random photo' data-title='Random photo'>`).join('');
+
+const imageSection = `
+  <section class='image-section' role='contentinfo' aria-label='Section with photos'>
+    ${imageElements}
+  </section>
+`;
+
+// Add all the HTML into one string
+export const HTMLString = header + about + imageSection;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
+import { HTMLString } from './HTMLString';
 import '../styles/stylesheet.css';
 
-// Create header
-const header = `
-  <header>
-    <h1>The Lazy Loader</h1>
-  </header>
-`;
-
-// Create description
-const about = `
-  <p class='description'>
-    The images will load when entering the viewport.
-  </p>
-`;
-
-// Create the image section
-const arrayOfImageNames = [];
-for (let i = 1; i <= 20; i++) { arrayOfImageNames.push(`${i}`);}
-const imageElements = arrayOfImageNames.map(element => `<img class='lazy-image' data-src=assets/${element}.png data-alt='Random photo' data-title='Random photo'>`).join('');
-
-const imageSection = `
-  <section class='image-section' role='contentinfo' aria-label='Section with photos'>
-    ${imageElements}
-  </section>
-`;
 
 // Insert sections in the DOM
-document.body.insertAdjacentHTML('afterbegin', header);
-document.querySelector('header').insertAdjacentHTML('afterend', about);
-document.body.insertAdjacentHTML('beforeend', imageSection);
+document.getElementById('app').insertAdjacentHTML('afterbegin', HTMLString);
 
 
 

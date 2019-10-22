f6f24df63bce600fc2ad2e5a3984c5175fc7a086
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-10-22 22:24:59 +0200

    Lazy loading of images with IntersectionObserver

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const about = `
 // Create the image section
 const arrayOfImageNames = [];
 for (let i = 1; i <= 20; i++) { arrayOfImageNames.push(`${i}`);}
-const imageElements = arrayOfImageNames.map(element => `<img class='image' src=assets/${element}.png alt='Random photo' title='Random photo'>`).join('');
+const imageElements = arrayOfImageNames.map(element => `<img class='lazy-image' data-src=assets/${element}.png data-alt='Random photo' data-title='Random photo'>`).join('');
 
 const imageSection = `
   <section class='image-section' role='contentinfo' aria-label='Section with photos'>
@@ -29,3 +29,47 @@ const imageSection = `
 document.body.insertAdjacentHTML('afterbegin', header);
 document.querySelector('header').insertAdjacentHTML('afterend', about);
 document.body.insertAdjacentHTML('beforeend', imageSection);
+
+
+
+
+let images;
+
+const options = {
+  root: null,
+  rootMargin: '20px',
+  threshold: 1.0
+};
+
+let imageObserver;
+
+function onIntersectionEvent(images) {
+  // Loop through the entries
+  images.forEach(entry => {
+    // Are we in viewport?
+    if (entry.isIntersecting) {
+      // Stop watching and load the image
+      //preloadImage(entry.target);
+      console.log(entry)
+      const image = entry.target
+      image.src = image.dataset.src
+      image.alt = image.dataset.alt
+      image.title = image.dataset.title
+      imageObserver.unobserve(entry.target);
+    }
+  });
+}
+
+window.addEventListener('load', function() {
+  images = document.querySelectorAll('.lazy-image');
+  createObserver(options);
+}, false);
+
+function createObserver(options) {
+  imageObserver = new IntersectionObserver(onIntersectionEvent, options);
+  images.forEach(image => {
+    imageObserver.observe(image);
+  });
+};
+
+

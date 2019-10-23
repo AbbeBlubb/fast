cb31f96c6af1f839da30b501f5b18ceb32146381
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-10-23 20:49:17 +0200

    Refactoring of the image observer

---
+++ src/imageObserver.js
@@ -0,0 +1,45 @@
+class ImageObserverCreator {
+
+  constructor() {
+    this.images;
+    this.imageObserver;
+    this.options = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1.0
+    };
+
+    window.addEventListener('load', this.onDOMContentLoaded.bind(this), false);
+  };
+
+  createObserver(options) {
+    this.imageObserver = new IntersectionObserver(this.onIntersectionEvent, options);
+    this.images.forEach(image => {
+      this.imageObserver.observe(image);
+    });
+  };
+
+  onDOMContentLoaded() {
+    this.images = document.querySelectorAll('.lazy-image');
+    this.createObserver(this.options);
+  }
+
+   onIntersectionEvent(images, imageObserver) {
+    // Loop through the images
+    images.forEach(entry => {
+      // Is the image in viewport?
+      if (entry.isIntersecting) {
+        // Load the image and stop watching. The image is accessible in entry.target
+        console.log(entry);
+        const image = entry.target;
+        image.src = image.dataset.src;
+        image.alt = image.dataset.alt;
+        image.title = image.dataset.title;
+        // Because of 'this', imageObserver must be fed as arg to parent function
+        imageObserver.unobserve(entry.target);
+      }
+    });
+  }
+}
+
+export const imageObserverSingleton = new ImageObserverCreator();
--- src/index.js
+++ src/index.js
@@ -1,50 +1,7 @@
 import { HTMLString } from './HTMLString';
 import '../styles/stylesheet.css';
+import './imageObserver';
 
 
 // Insert sections in the DOM
 document.getElementById('app').insertAdjacentHTML('afterbegin', HTMLString);
-
-
-
-
-let images;
-
-const options = {
-  root: null,
-  rootMargin: '20px',
-  threshold: 1.0
-};
-
-let imageObserver;
-
-function onIntersectionEvent(images) {
-  // Loop through the entries
-  images.forEach(entry => {
-    // Are we in viewport?
-    if (entry.isIntersecting) {
-      // Stop watching and load the image
-      //preloadImage(entry.target);
-      console.log(entry)
-      const image = entry.target
-      image.src = image.dataset.src
-      image.alt = image.dataset.alt
-      image.title = image.dataset.title
-      imageObserver.unobserve(entry.target);
-    }
-  });
-}
-
-window.addEventListener('load', function() {
-  images = document.querySelectorAll('.lazy-image');
-  createObserver(options);
-}, false);
-
-function createObserver(options) {
-  imageObserver = new IntersectionObserver(onIntersectionEvent, options);
-  images.forEach(image => {
-    imageObserver.observe(image);
-  });
-};
-
-

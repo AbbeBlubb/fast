5f3a9972f7f9e2b1020b62fb07c8eff9e34b7d68
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-10-29 20:12:56 +0100

    Routing: on click and on back/forward

--- src/router.js
+++ src/router.js
@@ -1,79 +1,79 @@
 import {infiniteView} from './infinite';
 
-const Router = function(routerName, routesArray) {
-  return {
-    routerName,
-    routesArray
+
+
+
+    let app = null;
+    let activeRoutes = null;
+    let currentPath = window.location.pathname;
+
+    // Register rout info for each route that will decide content in the SPA
+    const routes = [
+      {
+        path: '/',
+        name: 'Root'
+      },
+      {
+        path: '/infinite',
+        name: 'Infinite'
+      },
+      {
+        path: '/fastness',
+        name: 'Fastness'
+      }
+    ];
+
+    // DOM -> Get the active attribute routes -> attatach event listeners -> on click -> pushState
+    window.addEventListener('DOMContentLoaded', function() {
+      app = document.getElementById('app');
+      // The DOM nodes needs to be converted to an array in order to have array methods
+      activeRoutes = Array.from(document.querySelectorAll('[route]'));
+      activeRoutes.forEach(function(route) {
+        route.addEventListener('click', navigate, false);
+      });
+    });
+
+    //WORKS - triggered only when pressing back-button (transition between two pages)
+    window.addEventListener('popstate', function(popstateEvent) {
+      console.log('pop', popstateEvent)
+      const location = window.location.pathname
+      console.log(location)
+      reactOnURLChange(location);
+    });
+
+
+  // When click on navigation button, take the event and set the new path in the URL bar
+  const navigate = function(event) {
+    const btnRoutePath = event.target.attributes[0].value;
+    const targetRoute = routes.filter(function(registeredRouteInfo) {
+      return registeredRouteInfo.path === btnRoutePath;
+    })[0];
+
+    if(!targetRoute) {
+      window.history.pushState({}, '', 'error');
+      reactOnURLChange('error');
+    } else {
+      window.history.pushState({}, '', targetRoute.path);
+      reactOnURLChange(targetRoute.path);
+    }
   };
-};
-
-// Register rout info for each route
-const projectRouter = new Router ('projectRouter', [
-  {
-    path: '/',
-    name: 'Root'
-  },
-  {
-    path: '/infinite',
-    name: 'Infinite'
-  },
-  {
-    path: '/fastness',
-    name: 'Fastness'
-  }
-]);
-
-let app = null;
-let activeRoutes = null;
-
-// Set path in the URL bar
-function navigate(event) {
-  const eventRoutePath = event.target.attributes[0].value;
-  const routeDetailsFromRegisteredRoutes = projectRouter.routesArray.filter(function(routeObject) {
-    return routeObject.path === eventRoutePath;
-  })[0];
-
-  console.log(eventRoutePath, routeDetailsFromRegisteredRoutes);
-
-  if(!routeDetailsFromRegisteredRoutes) {
-    window.history.pushState({}, '', 'error')
-  }
-  else {
-    window.history.pushState({}, '', routeDetailsFromRegisteredRoutes.path)
-  }
-
-}
-
-// Get the active attribute routes to attatach event listeners
-window.addEventListener('DOMContentLoaded', function() {
-  app = document.getElementById('app');
-  // The DOM nodes needs to be converted to an array in order to have array methods
-  activeRoutes = Array.from(document.querySelectorAll('[route]'));
-  console.log(app);
-
-  activeRoutes.forEach(function(route) {
-    route.addEventListener('click', navigate, false);
-  });
-});
-
-let currentPath = window.location.pathname;
 
 // React when URL bar changes, needs litsener . App is null if the value is not set after dom content loaded
-switch (currentPath) {
-  case '/':
-    //app.outerHTML = 'will render root';
-    break;
-  case '/infinite':
-    app = document.getElementById('app');
-    app.outerHTML = infiniteView;
-    break;
-  case '/fastness':
-    app = document.getElementById('app');
-    app.outerHTML = 'fastness';
-    break;
-  default:
-    app = document.getElementById('app');
-    app.outerHTML = '404';
-}
+  function reactOnURLChange(currentPath) {
+    //app = document.getElementById('app');
+    switch (currentPath) {
+      case '/':
+        app.innerHTML = `<div id='app'>Root</div>`;
+        break;
+      case '/infinite':
+        app.innerHTML = `<div id='app'>Infinite</div>`;
+        break;
+      case '/fastness':
+        app.innerHTML = `<div id='app'>Fastness</div>`;
+        break;
+      default:
+        app.innerHTML = `<div id='app'>404</div>`;
+    }
+  }
 
-console.log(currentPath)
+

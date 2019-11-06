d30317725a952ce6caf48b50be9659a5a4ae510e
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-11-06 22:31:56 +0100

    Tile text

--- src/templates/infinite/tile.js
+++ src/templates/infinite/tile.js
@@ -2,7 +2,7 @@ export const tile = indexNumber => `
   <li class='target-placeholder' id='tile-${indexNumber}'>
     <div class='target-placeholder__background'>
       <div class='target-placeholder__vertical-line'></div>
-      <div class='target-placeholder__rectangle-first'>${indexNumber}</div>
+      <div class='target-placeholder__rectangle-first'>Recyclable DOM element ${indexNumber} of 20<br>Showing index item X of 200</div>
       <div class='target-placeholder__horizontal-line-second'></div>
       <div class='target-placeholder__horizontal-line-third'></div>
     </div>

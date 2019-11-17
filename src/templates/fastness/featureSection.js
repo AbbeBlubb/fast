837a358633cd03976b1c881a1278f19d47162eb1
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-11-17 17:52:50 +0100

    Description of the project

--- src/templates/fastness/featureSection.js
+++ src/templates/fastness/featureSection.js
@@ -1,6 +1,42 @@
 export const featureSection = `
   <section class='feature-section' role='contentinfo' aria-label='xxx' id='content'>
+
     <h2>Features</h2>
-    <p>Parraf</p>
+
+    <p>
+      The aim of this project is to give the fastest possible experience.
+      Some of the features are:
+    </p>
+
+    <ul>
+      <li>
+        The project is built in plain JavaScript, meaning the code is as lightweight it can be -
+        thus avoiding the need for the client to load a framework.
+      </li>
+      <li>
+        For this reason, I built my own routing and an event bus.
+      </li>
+      <li>
+        The image loader uses intersection observer to load images as they are needed,
+        thus avoiding to load unnessesary content.
+        You can follow the loading in the inspector network tab.
+      </li>
+      <li>
+        The infinite scroll uses intersection observer to re-calculate the position of the elements cluster and to update the content of the elements.
+        This way the DOM only contains 20 elements - light and fast to process, in contrast to having a very large DOM full of content.
+        The user is given a seamless scrolling experience.
+        You can follow the recycling in the inspector elements panel.
+      </li>
+      <li>
+        Webpack is configured for development and production.
+        The configuration includes code-splitting to facilitate browser caching.
+      </li>
+    </ul>
+
+    <p>
+      All this gives the user a fast-loading experience with minimum of code loading and processing.
+      The project is responsive and built as mobile first.
+    </p>
+
   </section>
 `;
--- src/templates/infinite/aboutInfinite.js
+++ src/templates/infinite/aboutInfinite.js
@@ -1,10 +1,6 @@
 export const aboutInfinite = `
   <section class='about' role='contentinfo' aria-label='About the project' id='about'>
     <p class='about-description'>
-      There's 20 DOM elements in the scroll section below.
-      Those elements will be recycled when scrolling, instead of adding elements to the DOM or reloading elements in the DOM.
-      When the 1:st or the 20:th element is intersecting the viewport,
-      the elements will be moved and the content of the entering elements will be changed.
-      This gives an effect of seamless infinite scroll at the same time the DOM is as light and fast as possible.
+      This infinite scroll recycles it's 20 DOM elements when scrolling: changing the contents and the positions.
     </p>
   </section>`;
